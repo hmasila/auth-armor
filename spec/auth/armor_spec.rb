8975c9efab2c2ae696dabb52aa000d20b34df98f
msabeni <kemimsabeni@gmail.com>
2021-01-18 11:39:54 +0300

    rubocop changes

--- spec/auth/armor_spec.rb
+++ spec/auth/armor_spec.rb
@@ -5,16 +5,16 @@ RSpec.describe Auth::Armor do
     expect(Auth::Armor::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
+  #it "does something useful" do
+  #  expect(false).to eq(true)
+  #end
 
-describe connect do
+  describe connect do
   it "Connects successfully" do
     expect{200}.to output{"Connection successfull"}.to_stdout
   end  
   it "invalid connection" do 
-     expect{400}.to output{"Error"}.to_stdout
+    expect{400}.to output{"Error"}.to_stdout
   end
   it "fails" do
     expect{response.to_str}.to output{"Connection failed"}.to_stdout
@@ -33,13 +33,12 @@ describe auth-requests do
 
 describe  invite_request do
   it "has a valid invite request" do
-   expect( invite_request)not_to be nil
+   expect( invite_request).not_to be_nil
  end
 
 describe generate_qr_code do
   it "has a valid qr code" do
-   expect(generate_qr_code)not_to be nil
+   expect(generate_qr_code).not_to be_nil
   end
  end
 end
-
